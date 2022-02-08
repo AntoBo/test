457a0e80b519629da274680c1cfc1fe1d4a3a1ff
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-08 16:37:16 +0200

    1 module HW done

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// console.log("test");
+
+// вопросы:
+// как происходит типизация данных в js?
+// difference between null - undefined?
+
+// const hello = "hello to alert";
+// const input = confirm("how old are u?");
+// alert(hello);
+
+// prompt(intup);
+
+// console.log(input);
+
+// const userName = "Вася";
+// const pow = 2;
+
+// const res = num % pow;
+
+// console.log(Math);
+
+// console.log(`Hello, ${userName}`);
+
+// console.log(Boolean(-Infinity));
