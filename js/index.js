58b915a8825cab98d739be46fa93c4d014884c37
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-10 13:06:40 +0200

    коммит

--- js/index.js
+++ js/index.js
@@ -1,24 +1,5 @@
-// console.log("test");
+"use strict";
 
-// вопросы:
-// как происходит типизация данных в js?
-// difference between null - undefined?
+const arr = [1, 2, 3, 4];
 
-// const hello = "hello to alert";
-// const input = confirm("how old are u?");
-// alert(hello);
-
-// prompt(intup);
-
-// console.log(input);
-
-// const userName = "Вася";
-// const pow = 2;
-
-// const res = num % pow;
-
-// console.log(Math);
-
-// console.log(`Hello, ${userName}`);
-
-console.log(Boolean(-Infinity));
+console.log(arr);
