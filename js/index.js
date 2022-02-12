9efa730aa526f0f1ec7d9910f6121343a04fbae8
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-12 15:20:28 +0200

    objects

--- js/index.js
+++ js/index.js
@@ -1,5 +1,49 @@
 "use strict";
 
-const arr = [1, 2, 3, 4];
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+// let evenNumbers = [];
 
-console.log(arr);
+// for(let i = start; i <= end, i +=1 ){
+//   i % 2 === 0 ? evenNumbers.push(i) :0;
+// }
+
+// return evenNumbers;
+//     // Change code above this line
+//   }
+
+// const obj = {
+//   nameObj: "object",
+//   number: 1,
+//   length: 3,
+//   comma: true,
+// };
+
+// // console.log(obj.comma);
+
+// function fn(arg = null) {
+//   console.log("Im fn");
+// }
+
+// const res = fn();
+// console.log(res);
+
+// fn();
+
+// function fn(...args) {
+//   console.log(args);
+// }
+
+// fn(1, 3, "array_el");
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+//   fn: function () {
+//     console.table("fn inside obj");
+//   },
+// };
+
+// obj.fn();
+
+console.table([1, 2]);
