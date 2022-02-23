aacf85daabea23c1e64c73ef19bd6f4834ede943
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-23 11:55:46 +0200

    current

--- js/index.js
+++ js/index.js
@@ -1,49 +1,5 @@
-"use strict";
+'use strict';
+const listEl = document.querySelector('.list');
 
-// function getEvenNumbers(start, end) {
-//    // Change code below this line
-// let evenNumbers = [];
-
-// for(let i = start; i <= end, i +=1 ){
-//   i % 2 === 0 ? evenNumbers.push(i) :0;
-// }
-
-// return evenNumbers;
-//     // Change code above this line
-//   }
-
-// const obj = {
-//   nameObj: "object",
-//   number: 1,
-//   length: 3,
-//   comma: true,
-// };
-
-// // console.log(obj.comma);
-
-// function fn(arg = null) {
-//   console.log("Im fn");
-// }
-
-// const res = fn();
-// console.log(res);
-
-// fn();
-
-// function fn(...args) {
-//   console.log(args);
-// }
-
-// fn(1, 3, "array_el");
-
-// const obj = {
-//   a: 1,
-//   b: 2,
-//   fn: function () {
-//     console.table("fn inside obj");
-//   },
-// };
-
-// obj.fn();
-
-console.table([1, 2]);
+console.log((listEl.firstElementChild.classList = 'item'));
+console.log(listEl.children);
