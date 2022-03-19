1be737db1c1efc2dee25969bbb876cb5891f0c2a
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-19 21:42:20 +0200

    test fetch

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-const pokemonEl = qs
+const pokemonEl = document.querySelector('.pokemon-container');
 
-
-
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+fetch('https://pokeapi.co/api/v2/pokemon/199')
   .then(responce => {
     return responce.json();
   })
   .then(pokemon => {
     console.log(pokemon.sprites.front_default);
+    pokemonEl.innerHTML = `<img src="${pokemon.sprites.front_default}">`;
   })
   .catch(error => {
-    console.log(error);
+    console.log('pokemon is lost with error: ', error);
   });
